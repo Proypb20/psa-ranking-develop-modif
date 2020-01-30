8b2e54a644245d8769e73e99220bfaeac5b8812d
marcelo <marcelo@marcelo-HP-Laptop-15-bs1xx>
2020-01-29 22:59:00 -0300

    fix tournament - user extraservice

--- src/main/java/com/psa/ranking/service/UserExtraService.java
+++ src/main/java/com/psa/ranking/service/UserExtraService.java
@@ -22,75 +22,76 @@ import com.psa.ranking.service.mapper.UserExtraMapper;
 @Transactional
 public class UserExtraService {
 
-    private final Logger log = LoggerFactory.getLogger(UserExtraService.class);
+	private final Logger log = LoggerFactory.getLogger(UserExtraService.class);
 
-    private final UserExtraRepository userExtraRepository;
+	private final UserExtraRepository userExtraRepository;
 
-    private final UserExtraMapper userExtraMapper;
-    
-    private final UserService userService;
+	private final UserExtraMapper userExtraMapper;
 
-    public UserExtraService(UserExtraRepository userExtraRepository, UserExtraMapper userExtraMapper, UserService userService) {
-        this.userExtraRepository = userExtraRepository;
-        this.userExtraMapper = userExtraMapper;
-        this.userService = userService;
-    }
+	private final UserService userService;
 
-    /**
-     * Save a userExtra.
-     *
-     * @param userExtraDTO the entity to save.
-     * @return the persisted entity.
-     */
-    public UserExtraDTO save(UserExtraDTO userExtraDTO) {
-        log.debug("Request to save UserExtra : {}", userExtraDTO);
-        UserExtra userExtra = userExtraMapper.toEntity(userExtraDTO);
-        userExtra = userExtraRepository.save(userExtra);
-        return userExtraMapper.toDto(userExtra);
-    }
+	public UserExtraService(UserExtraRepository userExtraRepository, UserExtraMapper userExtraMapper,
+			UserService userService) {
+		this.userExtraRepository = userExtraRepository;
+		this.userExtraMapper = userExtraMapper;
+		this.userService = userService;
+	}
 
-    /**
-     * Get all the userExtras.
-     *
-     * @param pageable the pagination information.
-     * @return the list of entities.
-     */
-    @Transactional(readOnly = true)
-    public Page<UserExtraDTO> findAll(Pageable pageable) {
-        log.debug("Request to get all UserExtras");
-        return userExtraRepository.findAll(pageable)
-            .map(userExtraMapper::toDto);
-    }
+	/**
+	 * Save a userExtra.
+	 *
+	 * @param userExtraDTO the entity to save.
+	 * @return the persisted entity.
+	 */
+	public UserExtraDTO save(UserExtraDTO userExtraDTO) {
+		log.debug("Request to save UserExtra : {}", userExtraDTO);
+		UserExtra userExtra = userExtraMapper.toEntity(userExtraDTO);
+		userExtra = userExtraRepository.save(userExtra);
+		return userExtraMapper.toDto(userExtra);
+	}
 
+	/**
+	 * Get all the userExtras.
+	 *
+	 * @param pageable the pagination information.
+	 * @return the list of entities.
+	 */
+	@Transactional(readOnly = true)
+	public Page<UserExtraDTO> findAll(Pageable pageable) {
+		log.debug("Request to get all UserExtras");
+		return userExtraRepository.findAll(pageable).map(userExtraMapper::toDto);
+	}
 
-    /**
-     * Get one userExtra by id.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    @Transactional(readOnly = true)
-    public Optional<UserExtraDTO> findOne(Long id) {
-        log.debug("Request to get UserExtra : {}", id);
-        return userExtraRepository.findById(id)
-            .map(userExtraMapper::toDto);
-    }
+	/**
+	 * Get one userExtra by id.
+	 *
+	 * @param id the id of the entity.
+	 * @return the entity.
+	 */
+	@Transactional(readOnly = true)
+	public Optional<UserExtraDTO> findOne(Long id) {
+		log.debug("Request to get UserExtra : {}", id);
+		return userExtraRepository.findById(id).map(userExtraMapper::toDto);
+	}
 
-    /**
-     * Delete the userExtra by id.
-     *
-     * @param id the id of the entity.
-     */
-    public void delete(Long id) {
-        log.debug("Request to delete UserExtra : {}", id);
-        userExtraRepository.deleteById(id);
-    }
-    
-    @Transactional(readOnly = true)
-    public UserExtra getUserWithAuthorities() {
-        User user = Optional.of(userService.getUserWithAuthorities().orElseThrow(() -> new IllegalArgumentException("No hay usuario logueado"))).get();
-        UserExtra userExtra = Optional.of(userExtraRepository.findByUser(user)).orElseThrow(() -> new IllegalArgumentException("")).get();
-        log.debug(userExtra.toString());
-        return userExtra;        
-    }
+	/**
+	 * Delete the userExtra by id.
+	 *
+	 * @param id the id of the entity.
+	 */
+	public void delete(Long id) {
+		log.debug("Request to delete UserExtra : {}", id);
+		userExtraRepository.deleteById(id);
+	}
+
+	@Transactional(readOnly = true)
+	public UserExtra getUserWithAuthorities() {
+		User user = Optional.of(userService.getUserWithAuthorities()
+				.orElseThrow(() -> new IllegalArgumentException("No hay usuario logueado"))).get();
+		UserExtra userExtra = Optional.of(userExtraRepository.findById(user.getId()))
+				.orElseThrow(() -> new IllegalArgumentException("No existe el userExtra con los datos ingresados"))
+				.get();
+		log.debug(userExtra.toString());
+		return userExtra;
+	}
 }
